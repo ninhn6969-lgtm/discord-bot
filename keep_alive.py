33c84c453ac462dacd8149925ac9d1f1241248e9
ninhn6969-lgtm <ninhn6969@gmail.com>
2026-05-11 19:24:46 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "I'm alive"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
@@ -13,3 +13,6 @@ def run():
 def keep_alive():
     t = Thread(target=run)
     t.start()
+
+# Gọi hàm này ngay trước khi bot.run()
+keep_alive()
